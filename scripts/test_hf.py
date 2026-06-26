6d6d01c15d6d4e18e65be205a191698f9c0e663b
JnrLe <jenner.lee.com@gmail.com>
2026-06-26 09:33:33 +0800

    Curate tracked study notes

--- scripts/test_hf.py
+++
@@ -1,7 +0,0 @@
-from datasets import load_dataset
-try:
-    ds = load_dataset("MIAMAI/TMMLUplus", "medical_license", split="test")
-    for i in range(3):
-        print(ds[i])
-except Exception as e:
-    print(e)
